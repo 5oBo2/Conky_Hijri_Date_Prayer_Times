6bfee284bac4e4b3e0520678bfe8197336e0d8ac
5oBo2 <i_laugh@live.com>
2025-02-08 23:12:24 +0200

    added time and date

--- Hijri_Date.lua
+++ Hijri_Date.lua
@@ -61,14 +61,33 @@ function conky_main()
     
 
     local font = "ITC Avant Garde Pro XLt"
-    local font_size = 500
-    local ypos = font_size
-    local margin = font_size/2.5
+    local font_size_time = 1000
+    local font_size_date = 300
+    local x_margin = font_size_date/2.5
+    local y_margin = font_size_date / 1.5
+    local ypos_date = font_size_time + font_size_date + y_margin
+    local ypos_hijri = ypos_date + font_size_date + y_margin / 2
+    -- print('min window height: ' .. ypos_hijri)
     local alpha = 200/255
 
     cr = cairo_create(cs)
     cairo_select_font_face(cr, font, CAIRO_FONT_SLANT_NORMAL, CAIRO_FONT_WEIGHT_NORMAL)
-    cairo_set_font_size(cr, font_size)
+    cairo_set_font_size(cr, font_size_time)
+    local extents = cairo_text_extents_t:create()
+
+    local date = os.date("%A |  %d %B %Y")
+    local time = os.date("%H%M")
+
+    cairo_text_extents(cr, time, extents)
+    local time_width = extents.width
+    local xpos_time = (conky_window.width - time_width) / 2 
+    draw_colored_text(cr, time, xpos_time, font_size_time, 1, 1, 1, alpha)  
+
+    cairo_set_font_size(cr, font_size_date)
+    cairo_text_extents(cr, date, extents)
+    local date_width = extents.width
+    local xpos_date = (conky_window.width - date_width) / 2 
+    draw_colored_text(cr, date, xpos_date, ypos_date, 1, 1, 1, alpha)  
 
     -- Ensure valid split
     if Day and Month and Year then
@@ -78,9 +97,8 @@ function conky_main()
         local day_red, day_green, day_blue, day_alpha = 1, 1, 1, alpha -- Default color: white
         if Day > 11 and Day < 16 then
             day_red, day_green, day_blue, day_alpha = 200/255, 55/255, 55/255, alpha -- Highlight color: red
-        end
-        
-        local extents = cairo_text_extents_t:create()
+        end        
+
         cairo_text_extents(cr, StringDay, extents)
         local day_width = extents.width
         cairo_text_extents(cr, Month, extents)
@@ -88,19 +106,19 @@ function conky_main()
         cairo_text_extents(cr, Year, extents)
         local year_width = extents.width
 
-        local total_width = day_width + month_width + year_width + (margin*2)
+        local total_width = day_width + month_width + year_width + (x_margin*2)
         local xpos = (conky_window.width - total_width) / 2 
 
         -- Draw the day with conditional color
-        draw_colored_text(cr, StringDay, xpos, ypos, day_red, day_green, day_blue, day_alpha)
+        draw_colored_text(cr, StringDay, xpos, ypos_hijri, day_red, day_green, day_blue, day_alpha)
 
         -- Draw the month in default color
-        xpos = xpos + day_width + margin
-        draw_colored_text(cr, Month, xpos, ypos, 1, 1, 1, alpha)        
+        xpos = xpos + day_width + x_margin
+        draw_colored_text(cr, Month, xpos, ypos_hijri, 1, 1, 1, alpha)        
         
         -- Draw the year in default color
-        xpos = xpos + month_width + margin
-        draw_colored_text(cr, tostring(Year), xpos, ypos, 1, 1, 1, alpha)        
+        xpos = xpos + month_width + x_margin
+        draw_colored_text(cr, tostring(Year), xpos, ypos_hijri, 1, 1, 1, alpha)        
   
 
     end
